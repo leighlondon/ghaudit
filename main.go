4d619a6f7f03c39f7b07ceca3969af70acd92e28
Leigh London <leigh.00.london@gmail.com>
2020-07-07 22:33:22 +1000

    listing collaborators

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"golang.org/x/oauth2"
 )
 
+type result struct {
+	repo string
+	user string
+}
+
 func main() {
 	token, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok || token == "" {
@@ -25,21 +30,35 @@ func main() {
 		ListOptions: github.ListOptions{PerPage: 50},
 		Affiliation: "owner",
 	}
-	var results []*github.Repository
+	var results []result
 	for {
-
 		repos, rsp, err := client.Repositories.List(ctx, "", &opts)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "unable to list repos: %s\n", err)
 			os.Exit(1)
 		}
-		results = append(results, repos...)
+		for _, repo := range repos {
+			name := repo.GetFullName()
+			collabs, _, err := client.Repositories.ListCollaborators(ctx, *repo.Owner.Login, *repo.Name, nil)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "unable to list collabs for %s: %s\n", name, err)
+				os.Exit(1)
+			}
+			for _, c := range collabs {
+				if *c.Login == *repo.Owner.Login {
+					continue
+				}
+				results = append(results, result{repo: name, user: *c.Login})
+			}
+		}
+
 		if rsp.NextPage == 0 {
 			break
 		}
 		opts.Page = rsp.NextPage
 	}
+
 	for _, r := range results {
-		fmt.Fprintf(os.Stdout, "%s\n", r.GetFullName())
+		fmt.Fprintf(os.Stdout, "%s,%s\n", r.repo, r.user)
 	}
 }
